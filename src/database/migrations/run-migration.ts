2c9d11b594541cb38dc19487bc0bd5b8bb6125b3
prathamps <pspratham16@gmail.com>
2025-08-22 19:35:22 +0530

    Error handling for migration

--- src/database/migrations/run-migration.ts
+++ src/database/migrations/run-migration.ts
@@ -1,10 +1,18 @@
+import pool from "../connection"
 import createContactsTable from "./001_create_contacts_table"
 import createIdentifyProcedure from "./002_create_identify_procedure"
 
 const runMigrations = async () => {
-	await createContactsTable()
-	await createIdentifyProcedure()
-	console.log("Migrations completed successfully.")
+	try {
+		await createContactsTable()
+		await createIdentifyProcedure()
+		console.log("Migrations completed successfully.")
+		pool.end()
+	} catch (error) {
+		console.error("Migration failed:", error)
+		pool.end()
+		process.exit(1)
+	}
 }
 
 runMigrations()
